7bdc31b7415adf1bc535378d9a8ec90352bd7ad6
Ledagra <svanluven90@gmail.com>
2023-05-11 12:50:50 -0700

    Current weather implementation

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use('/auth', require('./routes/register.js'));
 app.use('/auth', require('./routes/signin.js'));
 
 app.use('/verify', require('./routes/verify-email.js'));
+
+app.use('/weatherCurrent', require('./routes/Weather Routes/weatherCurrent.js'))
 /*
  * Return HTML for the / end point.
  * This is a nice location to document your web service API
