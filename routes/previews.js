45a6aac082d56741c95456a17882880086ef8357
Freddy1381 <zxu725@uw.edu>
2023-06-04 23:49:22 -0700

    pushme: enabled preview with no chat rooms

--- routes/previews.js
+++ routes/previews.js
@@ -24,31 +24,31 @@ let isStringProvided = validation.isStringProvided
  * 
  * @apiUse JSONError
  */ 
-router.get('/',(request, response, next) => {
-    let query = 'SELECT chatid, Name FROM Chats WHERE chatid in (SELECT chatid FROM chatmembers WHERE memberid = $1) ORDER BY chatid';
-    let values = [request.decoded.memberid];
-    const chatrooms = []
+router.get('/',(request, response) => {
+    // let query = 'SELECT chatid, Name FROM Chats WHERE chatid in (SELECT chatid FROM chatmembers WHERE memberid = $1) ORDER BY chatid';
+    // let values = [request.decoded.memberid];
+    // const chatrooms = []
 
-    pool.query(query, values)
-        .then(result => {
-            if (result.rowCount > 0) {
-                for (let i = 0; i < result.rowCount; i++) {
-                    chatrooms[i] = result.rows[i]
-                }
-                request.rooms = chatrooms
-                next()
-            } else {
-                response.status(400).send({
-                    message: "No chat room found."
-                })
-            }
-        }).catch(error => {
-            response.status(400).send({
-                message: "SQL Error at get chat name",
-                error: error
-            });
-        });
-}, (request, response) => {
+    // pool.query(query, values)
+    //     .then(result => {
+    //         if (result.rowCount > 0) {
+    //             for (let i = 0; i < result.rowCount; i++) {
+    //                 chatrooms[i] = result.rows[i]
+    //             }
+    //             request.rooms = chatrooms
+    //             next()
+    //         } else {
+    //             response.status(400).send({
+    //                 message: "No chat room found."
+    //             })
+    //         }
+    //     }).catch(error => {
+    //         response.status(400).send({
+    //             message: "SQL Error at get chat name",
+    //             error: error
+    //         });
+    //     });
+// }, (request, response) => {
     let query = `SELECT M.chatid, M.message, 
                  to_char(M.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS' ) AS Timestamp 
                  FROM Messages M 
